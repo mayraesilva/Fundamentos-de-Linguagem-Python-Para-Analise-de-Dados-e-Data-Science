253b00865c2fb6f4c170717f4be381f22c90f916
Mayra Silva <mayraeduarda2002@gmail.com>
2025-05-19 17:08:35 -0300

    simple calculator exercise

---
+++ calculadora_simples.py
@@ -0,0 +1,31 @@
+#Transformando pseudocódigo em códifo
+#Atividade pseudocódigo 2 da Data Science Academy por Mayra Silva
+
+def simple_calculator():
+    
+    print('Bem-vindo à Calculadora')
+
+    operand_1 = float(input('Insira o primeiro número: '))
+    operand_2 = float(input('Insira o segundo número: '))
+    operator = input('Selecione a operação (+, -, *, /): ')
+    result = 0
+
+
+
+    if operator == '+':
+        result = operand_1 + operand_2
+    
+    elif operator == '-':
+        result = operand_1 - operand_2
+
+    elif operator == '*':
+        result = operand_1 * operand_2
+
+    elif operator == '/':
+        result = operand_1 / operand_2
+
+    print(f'O resultado da operação é: {result}')
+
+    return
+
+simple_calculator()
