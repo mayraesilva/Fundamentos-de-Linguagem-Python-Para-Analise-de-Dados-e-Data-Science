b07850b94e7558779ef62436f3c380aa4cdc1dd5
Mayra Silva <mayraeduarda2002@gmail.com>
2025-05-26 16:36:55 -0300

    passo 2

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -6,11 +6,19 @@ from os import system, name
 #Função para limpar a tela a cada execução
 
 def limpa_tela():
-    #windows
+    # Windows
     if name =='nt':
         _ = system('cls')
 
     # Mac ou Linux
     else:
         _ = system('clear')
-        
+
+
+def jogo_da_forca():
+    limpa_tela()
+
+    print('\n Bem-vinda(o) ao jogo da forca!')
+    print('Adivinhe a palavra abaixo:\n')
+
+    palavras_possiveis = ['banana', 'uva', 'morango', 'manga', 'pera', ]
