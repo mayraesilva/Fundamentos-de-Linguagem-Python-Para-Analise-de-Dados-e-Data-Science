3c50e4fbc085d23889de847293f3d1d2aa2382ad
Mayra Silva <mayraeduarda2002@gmail.com>
2025-05-26 22:28:53 -0300

    passo 1

---
+++ projeto1.py
@@ -0,0 +1,39 @@
+# Projeto 1 - Fundamentos de Python ára Analise de dados por Mayra Silva
+
+import random
+from os import system, name
+
+
+def limpa_tela():
+
+    # Windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # Mac ou Linux
+    else:
+        _ = system('cls')
+
+
+
+
+def jogo_da_forca():
+
+    limpa_tela()
+
+
+    print('\nBem-vinda(o) ao jogo da forca!')
+    print('Adivinhe a palavra abaixo:\n')
+
+    possiveis_palavras = ['morango', 'laranja', 'uva']
+    chances = 6
+
+    palavra_escolhida = random.choice(possiveis_palavras)
+
+    qts_letras = '_ ' * len(palavra_escolhida)
+
+    print(qts_letras)
+
+
+
+jogo_da_forca()
