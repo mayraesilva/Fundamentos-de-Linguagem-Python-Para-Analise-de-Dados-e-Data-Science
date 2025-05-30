d31d72cd5d439a2a7327891b2fb7ab1ee77496cc
Mayra Silva <mayraeduarda2002@gmail.com>
2025-05-30 15:15:31 -0300

    escolhendo a palavra

--- Lab4-v1.py
+++ Lab4-v1.py
@@ -70,8 +70,37 @@ O   |
 class Hangman:
 
 	# Método Construtor
+    def __init__(self, file):
+          self.file = file
+          self.chances = 6
 
-	# Método para adivinhar a letra
+     
+    def transform_the_file(self): #para escolher as palavras de um arquivo
+          open_the_file = open(self.file, 'r', encoding='utf-8')
+          file_read = open_the_file.readlines()
+          #print('Este é o arquivo antes de remover os excessos', file_read)
+          file_modified = []
+
+          for line in file_read:
+               file_modified.append(line.strip()) #remove the space and the \n in the beggining/end of string
+
+          #print('Este é o arquivo após  a remoção ', file_modified)
+
+          self.word_list = file_modified
+          
+          return self.word_list
+
+
+    def choose_word(self):
+         self.secret_word = random.choice(self.word_list)
+         return self.secret_word
+    
+ 
+
+    
+     
+
+	
 	
 	# Método para verificar se o jogo terminou
 		
@@ -80,4 +109,16 @@ class Hangman:
 	# Método para não mostrar a letra no board
 		
 	# Método para checar o status do game e imprimir o board na tela
+
+
+
+game = Hangman("Cowboy_Carter_Tracklist.csv")
+
+print(game.file)
+print('____________________________')
+lista_de_palavras = game.transform_the_file()
+#print(lista_de_palavras)
+
+palavra = game.choose_word()
+print(palavra)
 
