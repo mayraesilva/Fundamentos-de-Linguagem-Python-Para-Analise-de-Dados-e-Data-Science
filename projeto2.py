f4512857b49fa332159d2a9904f1f4d2553b2091
Mayra Silva <mayraeduarda2002@gmail.com>
2025-06-10 13:07:29 -0300

    pacotes necessários p2

--- projeto2.py
+++ projeto2.py
@@ -1,1 +1,10 @@
 #Projeto 2 do curso Fundamentos de Linguagem Python para Análise de Dados e Data Science por Mayra Silva
+#Análise exploratória de dados em python para a área de varejo
+
+#Pacotes necessários
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
